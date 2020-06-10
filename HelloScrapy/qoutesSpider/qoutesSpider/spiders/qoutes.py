069a66c1c511177b98cefb54df89a00115ade89c
Blessed Chianumba <62527934+MrFlygerian@users.noreply.github.com>
2020-06-10 15:20:12 +0100

    Real commit

--- HelloScrapy/qoutesSpider/qoutesSpider/spiders/qoutes.py
+++ HelloScrapy/qoutesSpider/qoutesSpider/spiders/qoutes.py
@@ -11,8 +11,5 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        quotes = response.xpath("//div[@class='quote']//span[@class='text']/text()").extract()
+        yield {'quotes': quotes}
